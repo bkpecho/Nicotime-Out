97ec48ef423d6ebd2208ac865ddd063fcfa722f8
BK Pecho <bkpecho05@gmail.com>
2021-11-11 16:21:02 +0800

    Genesis 1.1

---
+++ app/src/main/java/Common/SplashScreen.java
@@ -0,0 +1,52 @@
+package Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.nicotimeout.app.R;
+
+import User.UserProgress;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+
+    //variable
+    ImageView backgroundImage;
+
+    //animations
+    Animation sideAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        backgroundImage = findViewById(R.id.background_image);
+
+        //animations
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+
+        //set anim on elements
+        backgroundImage.setAnimation(sideAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), UserProgress.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },SPLASH_TIMER);
+    }
+}
