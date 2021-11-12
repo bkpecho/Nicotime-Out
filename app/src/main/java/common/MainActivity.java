ab5934efd18724def34c0450027d2208ce2b0efc
BK Pecho <bkpecho05@gmail.com>
2021-11-12 20:12:42 +0800

    Added Indicator for OnBoarding Screen

--- app/src/main/java/common/MainActivity.java
+++ app/src/main/java/common/MainActivity.java
@@ -1,9 +1,13 @@
 package common;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.nicotimeout.app.R;
 
@@ -13,16 +17,30 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private OnboardingAdapter onboardingAdapter;
-
+    private LinearLayout layoutOnboardingIndicators;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboardingIndicators);
+
+
         setupOnboardingItems();
         ViewPager2 onboardingViewPager = findViewById(R.id.onBoardingViewPager);
         onboardingViewPager.setAdapter(onboardingAdapter);
+
+        setupOnboardingIndicators();
+        setCurrentOnboardingIndicator(0);
+
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicator(position);
+            }
+        });
 
     }
 
@@ -49,6 +67,41 @@ public class MainActivity extends AppCompatActivity {
         onboardingItems.add(slideThree);
 
         onboardingAdapter = new OnboardingAdapter(onboardingItems);
+    }
+
+    private void setupOnboardingIndicators() {
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+
+    }
+
+    private void setCurrentOnboardingIndicator(int index) {
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutOnboardingIndicators.getChildAt(i);
+            if (i == index) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_active)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
     }
 
 }
