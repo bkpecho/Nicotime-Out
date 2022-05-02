7f2eb0d8331e23c236cfb50e1de8c328f482dd2a
bkpecho <bkpecho05@gmai.com>
2022-05-02 15:35:06 +0800

    NEED achievement (videos), Videos, Rehab Tab

--- app/src/main/java/com/nicotimeout/app/user/fifthFragment/fifthFragment.java
+++ app/src/main/java/com/nicotimeout/app/user/fifthFragment/fifthFragment.java
@@ -1,5 +1,9 @@
 package com.nicotimeout.app.user.fifthFragment;
 
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -13,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.nicotimeout.app.R;
 
@@ -20,18 +25,33 @@ import java.util.ArrayList;
 
 
 public class fifthFragment extends Fragment {
+
+    public static final String PREF_ACHIEVEMENTS_COUNTER = "achievementsCounter";
+
     RecyclerView recyclerView;
     ArrayList images, name;
+
+    Dialog ac_explorer;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getActivity().getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(0xFFdb3056);
-        }
         View view = inflater.inflate(R.layout.fragment_fifth, container, false);
+
+        //shared preferences achievements
+        SharedPreferences prefs_achievements = getActivity().getSharedPreferences(PREF_ACHIEVEMENTS_COUNTER, 0);
+        SharedPreferences.Editor achievements_editor = getActivity().getSharedPreferences(PREF_ACHIEVEMENTS_COUNTER, 0).edit();
+        long pref_explorer = prefs_achievements.getLong("pref_explorer", 0);
+
+        ac_explorer = new Dialog(getActivity());
+
+        if (pref_explorer == 0) {
+            ac_explorer();
+            achievements_editor.putLong("pref_explorer", 1);
+            achievements_editor.apply();
+        }
+
+
         recyclerView = view.findViewById(R.id.fifthFragment_recyclerView);
         images = new ArrayList();
         name = new ArrayList();
@@ -45,7 +65,23 @@ public class fifthFragment extends Fragment {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(helperAdapter);
 
+        Window window = getActivity().getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(0xFF299cfc);
+
         return view;
+    }
+
+    private void ac_explorer() {
+        ac_explorer.setContentView(R.layout.ac_explorer);
+        ac_explorer.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        ac_explorer.setCancelable(false);
+        ac_explorer.setCanceledOnTouchOutside(false);
+
+
+        Button button = ac_explorer.findViewById(R.id.button);
+        button.setOnClickListener(view -> ac_explorer.dismiss());
+        ac_explorer.show();
     }
 
     @Override
